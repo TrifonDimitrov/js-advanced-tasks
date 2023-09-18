7739d57b826ac29724cd0d19bf592b8e8f69d920
Trifon Dimitrov <dimitrov1trifon@gmail.com>
2023-09-18 14:46:50 +0300

    Add files via upload
    Function that takes two positive numbers as input and compute the greatest common divisor.

---
+++ 2. greatestCommonDivisor-GCD.js
@@ -0,0 +1,15 @@
+function greatestCommonDivisorGCD(a, b) {
+
+    let GCD = a % b;
+
+    while (GCD !== 0) {
+        a = b;
+        b = GCD;
+        GCD = a % b;
+        
+    }
+
+    console.log(b);
+    
+}
+greatestCommonDivisorGCD(2154, 458);
